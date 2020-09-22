7074fe7ba8259d1aca571689474cd1bb7b4bd0a5
Ник Николос <nic23172@gmail.com>
2020-09-22 11:16:53 +0300

    Добавьте файлы проекта.

---
+++ htmlCss/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace htmlCss.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
